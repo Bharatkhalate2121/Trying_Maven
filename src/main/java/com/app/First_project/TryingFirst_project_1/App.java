f21fa9917747bc5b90c42629dfbdaa1de1a01794
bharatkhalate50 <bharatkhalate50@gmail.com>
2024-11-21 00:53:01 +0530

    v1

---
+++ src/main/java/com/app/First_project/TryingFirst_project_1/App.java
@@ -0,0 +1,10 @@
+package com.app.First_project.TryingFirst_project_1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
